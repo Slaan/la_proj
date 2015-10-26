925eab23d769af1b7763cf952334f7f2769313dc
Slaan <de.SlannesH@gmail.com>
2015-10-26 13:20:33 +0100

    Add Config and Log

---
+++ vindinium-client/src/main/java/bot/Config.java
@@ -0,0 +1,34 @@
+package bot;
+
+/**
+ * Created by slaan on 26.10.15.
+ */
+public class Config {
+
+    private Config() {}
+
+    public static String getName() {return null;}
+
+    public static int getNoOfRounds() {return 0;}
+
+    public static int getNoOfThreads() {return 0;}
+
+    public static String getAPIKey() {return null;}
+
+    public static String getSlackULR() {return null;}
+
+    public static String getDBUser() {return null;}
+
+    public static String getDBPassword() {return null;}
+
+    public static double getLearningRate() {return 0.0;}
+
+    public static double getExplorationRate() {return 0.0;}
+
+    public static double getDiscountFactor() {return 0.0;}
+
+    public static double getLambda() {return 0.0;}
+
+    public static int getQueueLenght() {return 0;}
+
+}
---
+++ vindinium-client/src/main/java/persistence/GameLog.java
@@ -0,0 +1,25 @@
+package persistence;
+
+/**
+ * Created by slaan on 26.10.15.
+ */
+public class GameLog {
+
+    public int gameID;
+
+    public boolean win;
+
+    public int rounds;
+
+    public int tavern;
+
+    public int mine;
+
+    public int deaths;
+
+    public int kills;
+
+    public int blockedWay;
+
+
+}
