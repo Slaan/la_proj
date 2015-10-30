a0df378957c05e08d85bd058ee82250c4c5f873f
Slaan <de.SlannesH@gmail.com>
2015-10-30 17:25:16 +0100

    Add ConfigSupport

--- vindinium-client/src/main/java/bot/Config.java
+++ vindinium-client/src/main/java/bot/Config.java
@@ -1,34 +1,94 @@
 package bot;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created by slaan on 26.10.15.
  */
 public class Config {
 
+    static String name;
+    static int NoOfRounds;
+    static int NoOfThreads;
+    static String APIKey;
+    static String SlackULR;
+    static String DBUser;
+    static String DBPassword;
+    static String Mode;
+    static Double LearningRate;
+    static Double ExplorationRate;
+    static Double DiscountFactor;
+    static Double Lamda;
+    static int QueueLength;
+    static InputStream inputstream=null;
+
+
     private Config() {}
 
-    public static String getName() {return null;}
+    public static void init() {
+        Properties prop = new Properties();
+        String propFileName = "res/mainconfig.properties";
 
-    public static int getNoOfRounds() {return 0;}
+        try {
+            inputstream = new FileInputStream(propFileName);
 
-    public static int getNoOfThreads() {return 0;}
+            prop.load(inputstream);
 
-    public static String getAPIKey() {return null;}
+            name = prop.getProperty("name");
+            NoOfRounds = Integer.parseInt(prop.getProperty("rounds"));
+            NoOfThreads = Integer.parseInt(prop.getProperty("threads"));
+            APIKey = prop.getProperty("apikey");
+            SlackULR = prop.getProperty("slackurl");
+            DBUser = prop.getProperty("dbuser");
+            DBPassword = prop.getProperty("dbpassword");
+            Mode = prop.getProperty("modus");
+            LearningRate = Double.parseDouble(prop.getProperty("learningrate"));
+            ExplorationRate = Double.parseDouble(prop.getProperty("explorationrate"));
+            DiscountFactor = Double.parseDouble(prop.getProperty("discountfactor"));
+            Lamda = Double.parseDouble(prop.getProperty("lamda"));
+            QueueLength = Integer.parseInt(prop.getProperty("queuelength"));
 
-    public static String getSlackULR() {return null;}
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (inputstream!=null) {
+                try {
+                    inputstream.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
 
-    public static String getDBUser() {return null;}
+    }
 
-    public static String getDBPassword() {return null;}
+    public static String getName() {return name;}
 
-    public static double getLearningRate() {return 0.0;}
+    public static int getNoOfRounds() {return NoOfRounds;}
 
-    public static double getExplorationRate() {return 0.0;}
+    public static int getNoOfThreads() {return NoOfThreads;}
 
-    public static double getDiscountFactor() {return 0.0;}
+    public static String getAPIKey() {return APIKey;}
 
-    public static double getLambda() {return 0.0;}
+    public static String getSlackULR() {return SlackULR;}
 
-    public static int getQueueLenght() {return 0;}
+    public static String getDBUser() {return DBUser;}
+
+    public static String getDBPassword() {return DBPassword;}
+
+    public static String getMode() {return Mode;}
+
+    public static double getLearningRate() {return LearningRate;}
+
+    public static double getExplorationRate() {return ExplorationRate;}
+
+    public static double getDiscountFactor() {return DiscountFactor;}
+
+    public static double getLambda() {return Lamda;}
+
+    public static int getQueueLenght() {return QueueLength;}
 
 }
---
+++ vindinium-client/src/main/java/bot/RewardConfig.java
@@ -0,0 +1,99 @@
+package bot;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by Daniel Hofmeister on 30.10.2015.
+ */
+public class RewardConfig {
+
+    static InputStream inputstream;
+
+    static int turnReward;
+    static int getMineReward;
+    static int blockedReward;
+    static int deathDefaultReward;
+    static int deathperMineReward;
+    static int deathForAllMinesReward;
+    static Double tavernPerHP;
+    static int tavernDefault;
+    static int killPerMine;
+    static int killPerMineDiscount;
+
+    public static void init() {
+        Properties prop = new Properties();
+        String propFileName = "res/mainconfig.properties";
+
+        try {
+            inputstream = new FileInputStream(propFileName);
+
+            prop.load(inputstream);
+
+            turnReward = Integer.parseInt(prop.getProperty("turn"));
+            getMineReward = Integer.parseInt(prop.getProperty("getMine"));
+            blockedReward = Integer.parseInt(prop.getProperty("blocked"));
+            deathDefaultReward = Integer.parseInt(prop.getProperty("deathDefault"));
+            deathperMineReward = Integer.parseInt(prop.getProperty("deathPerMine"));
+            deathForAllMinesReward = Integer.parseInt(prop.getProperty("deathForAllMines"));
+            tavernPerHP = Double.parseDouble(prop.getProperty("tavernPerHP"));
+            tavernDefault = Integer.parseInt(prop.getProperty("tavernDefault"));
+            killPerMine = Integer.parseInt(prop.getProperty("killPerMine"));
+            killPerMineDiscount = Integer.parseInt(prop.getProperty("killPerMineDiscount"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (inputstream!=null) {
+                try {
+                    inputstream.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public static int getTurnReward() {
+        return turnReward;
+    }
+
+    public static int getGetMineReward() {
+        return getMineReward;
+    }
+
+    public static int getBlockedReward() {
+        return blockedReward;
+    }
+
+    public static int getDeathDefaultReward() {
+        return deathDefaultReward;
+    }
+
+    public static int getDeathperMineReward() {
+        return deathperMineReward;
+    }
+
+    public static int getDeathForAllMinesReward() {
+        return deathForAllMinesReward;
+    }
+
+    public static Double getTavernPerHP() {
+        return tavernPerHP;
+    }
+
+    public static int getTarvernDefault() {
+        return tavernDefault;
+    }
+
+    public static int getKillPerMine() {
+        return killPerMine;
+    }
+
+    public static int getKillPerMineDiscount() {
+        return killPerMineDiscount;
+    }
+}
