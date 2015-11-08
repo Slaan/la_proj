bacea4dd94082e16788738c07931936ef41c3dd2
Simon K <Oct4v14n@googlemail.com>
2015-11-08 20:57:52 +0100

    Add values to all ENUMs.

--- vindinium-client/src/main/java/bot/Bender1/Distance.java
+++ vindinium-client/src/main/java/bot/Bender1/Distance.java
@@ -6,7 +6,21 @@ import bot.Bender0.RewardConfig;
  * Created by beckf on 06.11.2015.
  */
 public enum Distance {
-    BESIDE, CLOSE, MEDIUM, FAR;
+    BESIDE(0), CLOSE(1), MEDIUM(2), FAR(3);
+
+    private int value;
+    Distance(int value) { this.value = value; }
+    public int getValue() { return value; }
+
+    public static Distance fromValue(int value) {
+        switch (value) {
+            case 0: return BESIDE;
+            case 1: return CLOSE;
+            case 2: return MEDIUM;
+            case 3: return FAR;
+            default: throw new RuntimeException("Tried to get Distance from not supported value: " + value);
+        }
+    }
 
     public static Distance calcDistance(int distance){
         if(distance == 1){
--- vindinium-client/src/main/java/bot/Bender1/Quantity.java
+++ vindinium-client/src/main/java/bot/Bender1/Quantity.java
@@ -4,5 +4,17 @@ package bot.Bender1;
  * Created by slaan on 02.11.15.
  */
 public enum Quantity {
-    FEW, MIDDLE, LOTS;
+    FEW(0), MIDDLE(1), LOTS(2);
+
+    private int value;
+    private Quantity(int value) { this.value = value; }
+    public int getValue() { return value; }
+    public static Quantity fromValue(int value) {
+        switch (value) {
+            case 0: return FEW;
+            case 1: return MIDDLE;
+            case 2: return LOTS;
+            default: throw new RuntimeException("Tried to create a non supported Quantity: " + value);
+        }
+    }
 }
