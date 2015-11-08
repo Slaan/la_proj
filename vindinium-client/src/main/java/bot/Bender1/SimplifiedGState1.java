e1bbedf62bb6c3dd01cdfb6e5b81ad66a78be58a
Simon K <Oct4v14n@googlemail.com>
2015-11-08 22:22:30 +0100

    Add initial implementation of ID generation for Bender1.

--- vindinium-client/src/main/java/bot/Bender1/SimplifiedGState1.java
+++ vindinium-client/src/main/java/bot/Bender1/SimplifiedGState1.java
@@ -1,5 +1,6 @@
 package bot.Bender1;
 
+import algorithms.IdShifter;
 import algorithms.dijkstra.Dijkstra;
 import bot.Bender.ISimplifiedGState;
 import bot.Bender.Map;
@@ -10,6 +11,10 @@ import bot.dto.GameState;
  * Created by slaan on 02.11.15.
  */
 public class SimplifiedGState1 implements ISimplifiedGState {
+    static final int ID_SHIFT_DISTANCE = 3;
+    static final int ID_SHIFT_QUANTITY = 2;
+    static final int ID_SHIFT_DIRECTION = 3;
+    static final int ID_SHIFT_MINECOUNT = 1;
 
     private Quantity life;
     private int lifeHP;
@@ -95,8 +100,23 @@ public class SimplifiedGState1 implements ISimplifiedGState {
     }
 
     public int generateGStateId() {
-
-        return 0;
+        IdShifter id = new IdShifter();
+        id.shift(getLife().getValue(), ID_SHIFT_QUANTITY);
+        id.shift(getNoOfOurMines().getValue(), ID_SHIFT_QUANTITY);
+        id.shift(getClosestMine().getDirection().getValue(), ID_SHIFT_DIRECTION);
+        // TODO: implement closest hero.
+        //id.shift(getClosestHero().getDirection().getValue(), ID_SHIFT_DIRECTION);
+        id.shift(0, ID_SHIFT_DIRECTION);
+        id.shift(getClosestTavern().getDirection().getValue(), ID_SHIFT_DIRECTION);
+        id.shift(getClosestMine().getDistance().getValue(), ID_SHIFT_DISTANCE);
+        //id.shift(getClosestHero().getDistance().getValue(), ID_SHIFT_DISTANCE);
+        id.shift(0, ID_SHIFT_DISTANCE);
+        //id.shift(getClosestHero().getHealth().getValue(), ID_SHIFT_QUANTITY);
+        id.shift(0, ID_SHIFT_QUANTITY);
+        //id.shift(getClosestHero().getNoOfMines().getValue(), ID_SHIFT_MINECOUNT);
+        id.shift(0, ID_SHIFT_MINECOUNT);
+        id.shift(getClosestTavern().getDistance().getValue(), ID_SHIFT_DISTANCE);
+        return id.getId();
     }
 
     public int getLifeHP() {
