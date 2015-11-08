40746975177b6d99984b395e8556ada779db2efa
Slaan <de.SlannesH@gmail.com>
2015-11-08 18:04:55 +0100

    Add Map1 for Bender1 and Hero Tiles

--- vindinium-client/src/main/java/bot/Bender/TileType.java
+++ vindinium-client/src/main/java/bot/Bender/TileType.java
@@ -7,7 +7,7 @@ import bot.dto.GameState;
  */
 public enum TileType {
 
-    BLOCKED(0), MINE(1), TAVERN(2), FREE(3);
+    BLOCKED(0), MINE(1), TAVERN(2), FREE(3), HERO1(4), HERO2(5), HERO3(6), HERO4(7);
 
     private int value;
     private TileType(int value) { this.value = value; }
@@ -28,6 +28,11 @@ public enum TileType {
             case 1: return MINE;
             case 2: return TAVERN;
             case 3: return FREE;
+            case 4: return HERO1;
+            case 5: return HERO2;
+            case 6: return HERO3;
+            case 7: return HERO4;
+
             default: throw new RuntimeException("TileType " + value + " does not exist.");
         }
     }
@@ -41,6 +46,11 @@ public enum TileType {
             case MINE: return 'M';
             case TAVERN: return 'T';
             case FREE: return ' ';
+            case HERO1: return '1';
+            case HERO2: return '2';
+            case HERO3: return '3';
+            case HERO4: return '4';
+
             default: throw new RuntimeException("TileType has no Abbreviation: " + this);
         }
     }
---
+++ vindinium-client/src/test/java/bot/Map1Test.java
@@ -0,0 +1,54 @@
+package bot;
+
+import bot.Bender.Map;
+import bot.Bender.TileType;
+import bot.Bender1.Map1;
+import bot.dto.GameState;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by Daniel Hofmeister on 08.11.2015.
+ */
+public class Map1Test {
+
+    TileType[][] expectedMap;
+    TileType[][] realMap;
+    GameState gameState;
+
+    @Before
+    public void setup() {
+        int size = 3;
+        String tiles =
+                "##@1  " +
+                "$1[]$-" +
+                "$2  @2";
+        GameState.Position position = new GameState.Position(1,2);
+        GameState.Position spawn = new GameState.Position();
+        GameState.Hero hero = new GameState.Hero(1, "dan", "12", 2111, position, 100, 100, 3, spawn, false);
+        GameState.Board board = new GameState.Board(tiles, size);
+        GameState.Game game = new GameState.Game("1", 5, 220, null, board, false);
+        gameState = new GameState(game, hero, "bla", "blub", "blib");
+    }
+
+    @Test
+    public void parseMapTest() {
+        Map1 map = new Map1(gameState);
+        realMap = map.getCurrentMap();
+
+        expectedMap = new TileType[3][3];
+        expectedMap[0][0] = TileType.BLOCKED;
+        expectedMap[1][0] = TileType.HERO1;
+        expectedMap[2][0] = TileType.FREE;
+        expectedMap[0][1] = TileType.BLOCKED;
+        expectedMap[1][1] = TileType.TAVERN;
+        expectedMap[2][1] = TileType.MINE;
+        expectedMap[0][2] = TileType.MINE;
+        expectedMap[1][2] = TileType.FREE;
+        expectedMap[2][2] = TileType.HERO2;
+
+        assertArrayEquals(expectedMap,realMap);
+    }
+}
