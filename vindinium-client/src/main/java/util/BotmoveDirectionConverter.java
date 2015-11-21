df634f4a8448523af105fb1557d583e87c3bb5d5
Slaan <de.SlannesH@gmail.com>
2015-11-21 01:51:31 +0100

    Add util class with botmove/dir converter

---
+++ vindinium-client/src/main/java/util/BotmoveDirectionConverter.java
@@ -0,0 +1,43 @@
+package util;
+
+import bot.Bender.BotMove;
+import bot.Bender.DirectionType;
+
+/**
+ * Created by Daniel Hofmeister on 21.11.2015.
+ */
+public class BotmoveDirectionConverter {
+
+    public static DirectionType getDirectionTypeFromBotMove(BotMove move) {
+        DirectionType result = null;
+        if (move.equals(BotMove.EAST)) {
+            result = DirectionType.EAST;
+        } else if (move.equals(BotMove.SOUTH)) {
+            result = DirectionType.SOUTH;
+        } else if (move.equals(BotMove.WEST)) {
+            result = DirectionType.WEST;
+        } else if (move.equals(BotMove.NORTH)) {
+            result = DirectionType.NORTH;
+        } else {
+            throw new IllegalArgumentException("no such BotMove");
+        }
+        return result;
+    }
+
+    public static BotMove getBotMoveFromDirectionType(DirectionType dir) {
+        BotMove result = null;
+
+        if(dir.equals(DirectionType.EAST)) {
+            result = BotMove.EAST;
+        } else if(dir.equals(DirectionType.SOUTH)) {
+            result = BotMove.SOUTH;
+        } else if(dir.equals(DirectionType.WEST)) {
+            result = BotMove.WEST;
+        } else if(dir.equals(DirectionType.NORTH)) {
+            result = BotMove.NORTH;
+        } else {
+            throw new IllegalArgumentException("no such DirectionType");
+        }
+        return result;
+    }
+}
