d5bb36ddda0d7f88a868019cde8d0e4cb0d96c11
Simon K <Oct4v14n@googlemail.com>
2015-11-29 14:56:05 +0100

    Add c3p0 as Hibernate poolprovider.

--- vindinium-client/src/main/java/persistence/SessionBuilder.java
+++ vindinium-client/src/main/java/persistence/SessionBuilder.java
@@ -15,12 +15,19 @@ public class SessionBuilder {
         SessionFactory factory = null;
         try{
             java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
+            System.setProperty("com.mchange.v2.log.MLog", "com.mchange.v2.log.FallbackMLog");
+            System.setProperty("com.mchange.v2.log.FallbackMLog.DEFAULT_CUTOFF_LEVEL", "WARNING");
             factory = new AnnotationConfiguration().
                     configure().
                     setProperty("hibernate.connection.url", "jdbc:mysql://localhost/" + bender).
                     setProperty("hibernate.hbm2ddl.auto", "update").
                     setProperty("hibernate.connection.username", Config.getDBUser()).
                     setProperty("hibernate.connection.password", Config.getDBPassword()).
+                    setProperty("hibernate.c3p0.min_size", "5").
+                    setProperty("hibernate.c3p0.max_size", "200").
+                    setProperty("hibernate.c3p0.max_statements", "10").
+                    setProperty("hibernate.c3p0.idle_test_period", "0").
+                    setProperty("hibernate.c3p0.timeout", "20").
                     addAnnotatedClass(State.class).
                     addAnnotatedClass(StateAction.class).
                     addAnnotatedClass(GameLog.class).
